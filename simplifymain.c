b20892fb2fadfdfddff91f2a6074f649a5fd5a0b
Kai0522 <kklin0522@gmail.com>
2019-03-04 21:44:33 +0800

    My homeworks answer Ver2

--- simplifymain.c
+++ simplifymain.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int add(int a , int b){return a+b;}
-int sub(int a , int b){return a-b;}
-int mul(int a , int b){return a*b;}
-int divi(int a , int b){return a/b;}
+void add(int *a , int *b){*a = *a+*b;}
+void sub(int *a , int *b){*a = *a-*b;}
+void mul(int *a , int *b){*a = *a * *b;}
+void divi(int *a , int *b){*a = *a / *b;}
 
-int (*ptr[8])(int a , int b)={0,0,mul,add,0,sub,0,divi};
+void (*ptr[8])(int *a , int *b)={0,0,mul,add,0,sub,0,divi};
 
 int main(){
     char i;
@@ -14,6 +14,7 @@ int main(){
     printf("Key a Function \n EX:1+1\n");
     scanf("%i %c %i",&a,&i,&b);
     int index=i-'(';
-    int result=(*ptr[index])(a,b);
-    printf("%d \n",result);
+    printf("%d",a);
+    ptr[index](&a,&b);
+    printf(" %c %d = %d \n",i,b,a);
 }
